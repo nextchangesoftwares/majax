354d2613d2cd0bb127cea5bc3d586d3a95d87536
nextchangesoftwares <53474269+nextchangesoftwares@users.noreply.github.com>
2019-09-02 12:50:17 +0530

    Update majax.js

--- majax.js
+++ majax.js
@@ -1,4 +1,10 @@
-    function createTable()
+function doMultiply(p1, p2) {
+  var output=1;
+  output = p1 * p2;  
+  document.write(output)// The function returns the product of p1 and p2
+}
+
+function createTable()
     {
       var j=1;
       var output = "<table border='1' width='500' cellspacing='0'cellpadding='5'>";
