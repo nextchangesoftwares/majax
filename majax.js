c72829d01e41a5d8cb2cf63e2649a4bed0a92c54
nextchangesoftwares <53474269+nextchangesoftwares@users.noreply.github.com>
2019-09-02 14:42:14 +0530

    Update majax.js

--- majax.js
+++ majax.js
@@ -1,2 +1,14 @@
 document.write("<center><h1>Majax framework initialized</h1></center>");
 document.write("<center><img src='images/logo.png'></center>");
+if (str.length == 0) {
+    return;
+  } else {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.write(this.responseText);
+      }
+    };
+    xmlhttp.open("GET", "gethint.php?q=" + str, true);
+    xmlhttp.send();
+  }
