69784ea4d6f6faa928b6e680694d5befa0a4fbd1
nextchangesoftwares <53474269+nextchangesoftwares@users.noreply.github.com>
2019-09-02 12:19:29 +0530

    Update majax.js

--- majax.js
+++ majax.js
@@ -1,11 +1,11 @@
-    function createTable(10, 10)
+    function createTable()
     {
       var j=1;
       var output = "<table border='1' width='500' cellspacing='0'cellpadding='5'>";
-      for(i=1;i<=rows;i++)
+      for(i=1;i<=10;i++)
       {
     	output = output + "<tr>";
-        while(j<=cols)
+        while(j<=10)
         {
   		  output = output + "<td>" + i*j + "</td>";
    		  j = j+1;
